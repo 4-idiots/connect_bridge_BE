5d99c12014bebc9371a2f90519e4187efbeb8122
moonjipsa <moonjipsa@naver.com>
2022-01-22 00:17:07 +0900

    ignore file

--- src/main/java/com/idiotdb/loginpage/register/repository/RegisterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.idiotdb.loginpage.register.repository;
-
-import com.idiotdb.loginpage.register.data.entity.RegisterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterRepository extends JpaRepository<RegisterEntity, Long>{
-}
