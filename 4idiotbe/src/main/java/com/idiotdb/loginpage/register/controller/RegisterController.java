fef6aa4cebf9dd8bbef6b806a29cb99c967f7ff9
moonjipsa <moonjipsa@naver.com>
2022-01-25 22:52:08 +0900

    Spring Boot cors setting

--- 4idiotbe/src/main/java/com/idiotdb/loginpage/register/controller/RegisterController.java
+++ 4idiotbe/src/main/java/com/idiotdb/loginpage/register/controller/RegisterController.java
@@ -15,7 +15,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:8080")
 public class RegisterController {
 
     private final RegisterImpl registerService;
