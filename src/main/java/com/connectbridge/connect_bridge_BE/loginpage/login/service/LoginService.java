a73546b691ae88b8c4440f8fd14efecfac13195e
SH-Kim1999 <ktjrghks0914@gmail.com>
2022-02-08 01:54:57 +0900

    controller, service 작동확인

--- src/main/java/com/connectbridge/connect_bridge_BE/loginpage/login/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.connectbridge.connect_bridge_BE.loginpage.login.service;
-
-import com.connectbridge.connect_bridge_BE.loginpage.login.data.dto.LoginDto;
-import com.connectbridge.connect_bridge_BE.loginpage.login.data.entity.LoginEntity;
-
-public interface LoginService {
-
-    LoginEntity login(LoginDto dto) throws Exception;
-}
