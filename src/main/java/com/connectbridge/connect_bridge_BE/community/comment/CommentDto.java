2e111744faf2121b44aa416d8c4c626289283fb6
moonjipsa <moonjipsa@naver.com>
2022-04-12 19:00:27 +0900

    dto test

--- src/main/java/com/connectbridge/connect_bridge_BE/community/comment/CommentDto.java
+++ src/main/java/com/connectbridge/connect_bridge_BE/community/comment/CommentDto.java
@@ -1,4 +1,12 @@
 package com.connectbridge.connect_bridge_BE.community.comment;
 
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
 public class CommentDto {
 }
