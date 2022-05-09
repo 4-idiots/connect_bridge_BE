a5cc8f01cb78dd97de764a4fb57413ce27f54670
SH-Kim1999 <SH-Kim1999@gmail.com>
2022-05-09 13:29:53 +0900

    follow -> like

--- src/main/java/com/connectbridge/connect_bridge_BE/projectpage/repository/ProjectRepository.java
+++ src/main/java/com/connectbridge/connect_bridge_BE/projectpage/repository/ProjectRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
     ProjectEntity findByid(Long id);
+    ProjectEntity findByUserID(Long userID);
 }
